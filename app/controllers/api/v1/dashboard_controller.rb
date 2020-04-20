b58a8701b25e2112a7fef9236d77414b14ef30ff
Enzo Glauber <enzojob@gmail.com]>
2020-04-20 18:10:11 -0300

    search

--- app/controllers/api/v1/dashboard_controller.rb
+++ app/controllers/api/v1/dashboard_controller.rb
@@ -16,7 +16,9 @@ class Api::V1::DashboardController < ApplicationController
   def load_recommendations
     heard_categories = @recent_albums.map(&:category)
     if heard_categories.present?
-      @recommended_albums = Album.joins(:category, :songs).where(category: heard_categories).order("songs.played_count")
+      @recommended_albums = Album.joins(:category, :songs)
+                                .where(category: heard_categories)
+                                .order("songs.played_count")
                                 .select("distinct albums.*").limit(12)
     else
       @recommended_albums = Album.all.limit(12)
