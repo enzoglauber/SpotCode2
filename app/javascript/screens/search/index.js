cbcda4a2910ea846549ca36d5886a3433abc8f94
Enzo Glauber <enzojob@gmail.com]>
2020-05-18 14:41:33 -0300

    screens and footer

--- app/javascript/screens/search/index.js
+++ app/javascript/screens/search/index.js
@@ -1,9 +1,12 @@
 import React, { Fragment, useState } from 'react';
 
+import NavbarFooter from '../../components/common/navbar_footer';
+
 const SearchScreen = () => {
   return (
     <Fragment>
       <div>Search Screen</div>
+      <NavbarFooter />
     </Fragment>
   );
 }
